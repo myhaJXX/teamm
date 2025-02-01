1ec883b5ba65de3ddefafb85104ad66e161a85b4
Sergey H. <myahwhynot@gmail.com>
2025-02-01 18:39:05 +0300

    feat(structure): configurators, setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,40 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    ignores: ['build', 'node_modules', '*.config.js', '**/*.d.ts', '.next']
+  },
+  {
+    "settings": {
+      "react": {
+        "version": "detect"
+      }
+    }
+  },
+  {rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        "args": "all",
+        "argsIgnorePattern": "^_",
+        "caughtErrors": "all",
+        "caughtErrorsIgnorePattern": "^_",
+        "destructuredArrayIgnorePattern": "^_",
+        "varsIgnorePattern": "^_",
+        "ignoreRestSiblings": true
+      }
+    ],
+    "@typescript-eslint/no-require-imports": "off",
+    "eqeqeq": ["error", "always"],
+    "curly": ["error", "all"],
+    "react/react-in-jsx-scope": "off"
+  }},
 ];
-
-export default eslintConfig;
